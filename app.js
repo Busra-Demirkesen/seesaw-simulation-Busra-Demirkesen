2a46cc9e3945df22580937fd9e6a6323f168dc92
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-10-21 14:37:51 +0200

    feat(interaction): capture click coordinates only on the plank

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const seesawEl = document.querySelector('.seesaw');
 const leftTotalEl = document.getElementById('left-total');
+const rightTotalEl = document.getElementById('right-total');
+
+if(!seesawEl || !leftTotalEl || !rightTotalEl){
+    console.error('Required elements not found in DOM');
+}
+
+seesawEl.addEventListener('click', (e) =>{
+    const rect = seesawEl.getBoundingClientRect();
+    const localX = e.clientX - rect.left;
+
+    if (localX < 0 || localX > rect.width) return;
+    const pivotX= rect.width / 2 ;
+    const distanceFromPivot = localX - pivotX;
+
+    console.log({
+        localX: Math.round(localX),
+        pivotX:Math.round(pivotX),
+        distanceFromPivot: Math.round(distanceFromPivot)
+    });
+});
+
+
